b5eb94e7f99323635fd5c05910e05ff618b2144c
kosmosr <kosmosr@hotmail.com>
2019-04-05 21:25:59 +0800

    [U]Update 001 And README.md

---
+++ src/main/java/utils/Array.java
@@ -0,0 +1,11 @@
+package main.java.utils;
+
+/**
+ * @author zmh
+ * @date 2019-04-05 21:09
+ */
+public class Array {
+    public static int[] constructorArray(int... numbers) {
+        return numbers;
+    }
+}
